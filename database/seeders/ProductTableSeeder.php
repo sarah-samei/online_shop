f1801b65ea86ad397a446487fa2d29858024aeb6
sarah <sarah.00.samei@gmail.com>
2024-02-07 01:00:35 +0330

    Create seeder file.

--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -12,6 +12,20 @@ class ProductTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $jewellery_name=array('flower', 'diamond', 'star');
+        $brand_name=array('LV', 'LV', 'channel' );
+        $gold_type=array('golden', 'golden', 'white' );
+        $jewellery_type=array('ring', 'ring', 'ring');
+        $price=array('1000', '5000', '2500');
+
+        for($i=0;$i<3;$i++) {
+            \App\Models\Product::create([
+                "jewellery_name" => $jewellery_name[$i],
+                "brand_name" => $brand_name[$i],
+                "gold_type" => $gold_type[$i],
+                "jewellery_type" => $jewellery_type[$i],
+                "price" => $price[$i],
+            ]);
+        }
     }
 }
